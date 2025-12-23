a960655ba98f4f1ca811c36019e57965358b20b5
ravishka-bandara <ravishkabandara01@gmail.com>
2025-12-23 22:23:20 +0530

    feat: add data base configuration in database.php

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+//data base configuration
+$host = 'localhost';
+$dbname = 'contact_system';
+$username = 'root'   // default for xamp login
+$password = '';    // default password empty 
+
