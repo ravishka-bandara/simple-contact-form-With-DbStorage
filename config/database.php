3c6f822610a7265aac8d0358178324f1682e80d0
ravishka-bandara <ravishkabandara01@gmail.com>
2025-12-23 22:30:34 +0530

    feat: add connection and set error mode

--- config/database.php
+++ config/database.php
@@ -6,3 +6,14 @@ $dbname = 'contact_system';
 $username = 'root'   // default for xamp login
 $password = '';    // default password empty 
 
+// create the connection
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    //set error mode
+    $pdo->setAttribute(PDO::ATTR_ERRMODE_EXPECTION);
+    echo " databse connected successfully"; // need remove after testing
+} catch(PDOException $e){
+    die("connection failed: " .$e->getMessage());
+}
+?>
