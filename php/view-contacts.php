c5083ca3c8afbca58ca9e6a4d4124c68512479fe
ravishka-bandara <ravishkabandara01@gmail.com>
2025-12-25 14:36:04 +0530

    feat: add start session fetch all contacts and add basic html structure

--- php/view-contacts.php
+++ php/view-contacts.php
@@ -1,3 +1,25 @@
 <?php
 //database connection
 require_once 'config/database.php';
+
+//start the session :if needed
+ session_start();
+
+ //fetch all contacts from database
+ $sql = "SELECT * FROM contacts ORDER BY created_at DESC";
+ $stmt = $pdo->query($sql);
+ $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
